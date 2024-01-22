02ae73a15e1f622e99846913c5147ff806ca0b6e
Gabriel Ávila <gabrielavila04@protonmail.com>
2024-01-21 22:30:25 -0300

    feat: :boom: default settings

---
+++ src/components/picker/Picker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Picker() {
+  return <div>Picker</div>;
+}
+
+export default Picker;
---
+++ src/stories/Picker.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Picker from "../components/picker/Picker";
+
+export default {
+  title: "Data Display/Tooltips",
+};
+
+const Template = () => {
+  return <Picker />;
+};
+
+export const Tooltips = Template.bind({});
